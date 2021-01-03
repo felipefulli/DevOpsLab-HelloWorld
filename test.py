5b8b8310db3e75301c911e910bce983ab09805e1
Felipe Fulli <felipefulli@hotmail.com>
2021-01-03 23:21:12 +0000

    Erro no Deploy corrigito

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "xxxxxxxx")
+        self.assertRegex(result.data.decode(), "Erro no Deploy Resolvido")
 
 
 if __name__ == "__main__":
